1cac2be7755752de6e4e4170a6110b5d97d040a8
adityasachan24@gmail.com <adityasachan24@gmail.com>
2024-06-16 17:20:01 +0530

    added

--- stack/loop.java
+++ stack/loop.java
@@ -2,15 +2,17 @@ package stack;
 
 import java.util.Stack;
 
-public class stack {
+public class loop {
+
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
         stack.push(1);
         stack.push(3);
         stack.push(5);
-        System.out.println(stack.isEmpty());
-        System.out.println(stack.pop());
-        System.out.println(stack);
-        System.out.println(stack.peek());   
-    }
+        while(!stack.isEmpty())//empty1
+        {
+            int value=stack.pop();
+            System.out.println(value);
+        }
+}
 }
