de1b18ee6523ad49045201ccb85fc0701693fb1d
adityasachan24@gmail.com <adityasachan24@gmail.com>
2024-06-18 11:22:16 +0530

    files added

---
+++ stack/stack1.java
@@ -0,0 +1,41 @@
+package stack;
+
+public class stack1 {
+    private int[] items;
+    private int top;
+    private int size;
+
+    public stack1(int size) {
+        this.size = size;
+        this.items = new int[size];
+        this.top = 0;
+    }
+
+    public void push(int value) {
+        if (top == size) {
+            System.out.println("Stack is full");
+            return;
+        }
+        this.items[this.top] = value;
+        this.top++;
+    }
+
+    public String toString() {
+        StringBuilder sc = new StringBuilder();
+        for (int i = top - 1; i >= 0; i--) {
+            sc.append(items[i]).append(" ");
+        }
+        return sc.toString();
+    }
+
+    public static void main(String args[])
+    {
+        stack1 stack = new stack1(5);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        System.out.println(stack);
+    }
+}
