09b0c9aebe7ab693d7865aaef905cc7e46e2f773
adityasachan24@gmail.com <adityasachan24@gmail.com>
2024-07-26 10:39:28 +0530

    preorder 0(N)

---
+++ trees/trees/traversal.java
@@ -0,0 +1,52 @@
+package trees;
+
+
+public class traversal {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTreeHelper {
+        static int index = -1;
+
+        public static Node buildTree(int[] nodes) {
+            return buildTreeHelper(nodes);
+        }
+
+        private static Node buildTreeHelper(int[] nodes) {
+            index++;
+            if (index >= nodes.length || nodes[index] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTreeHelper(nodes);
+            newNode.right = buildTreeHelper(nodes);
+            return newNode;
+        }
+    }
+
+//preorder --root ,left,right
+public static void preorder(Node root)
+{
+    if(root==null)
+    return ;
+System.out.print(root.data+"");
+preorder(root.left);
+preorder(root.right);
+}
+
+    public static void main(String[] args) {
+        int[] nodes = {1, 2, 3, 4, 5, -1, 2, 4, 7, 9, 0, 1, -1, -1};
+        Node root = BinaryTreeHelper.buildTree(nodes);
+        System.out.println(root.data);
+        preorder(root);
+    }
+}
